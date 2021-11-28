df797368113195ca81c63886fa24cebc09eb331a
div1085 <89940688+div1085@users.noreply.github.com>
2021-11-28 01:09:49 -0500

    updating file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,6 +4,25 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// add circle to map
+// let circle= L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#ffffa1',
+//     fillOpacity: 0.5,
+//     radius: 300
+//  }).addTo(map);
+
+// Add circlemarlker to map 
+let circle= L.circleMarker([34.0522, -118.2437],{
+    color: 'black',
+    fillColor: '#ffffa1',
+    fillOpacity: 0.5,
+    radius: 300
+}).addTo(map);
+
 // add tile layer 
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
